defe0d5f2fc3b8f299733e368fbabb775fc5a8f6
Yusuke Kawamoto <novogrammer@gmail.com>
2024-06-27 20:58:09 +0900

    three導入

--- src/App.ts
+++ src/App.ts
@@ -1,3 +1,5 @@
+import * as THREE from "three";
+
 export interface LatLng{
   lat:number,
   lng:number,
@@ -27,7 +29,29 @@ export default class App{
         lng:Math.max(a.lng,b.lng),
       }
     })
-
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setAnimationLoop( animate );
+    this.sectionElement.appendChild( renderer.domElement );
+    
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    
+    camera.position.z = 5;
+    
+    function animate() {
+    
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+    
+      renderer.render( scene, camera );
+    
+    }
   }
 
 }
